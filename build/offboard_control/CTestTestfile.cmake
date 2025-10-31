573df6a4e22a22decb4bc8d9af61781b3f166b0d
321code <haijun__chen@163.com>
2025-10-31 10:52:16 +0800

    add offboard_control

---
+++ build/offboard_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/attack_ws/src/offboard_control
+# Build directory: /home/ubuntu/attack_ws/build/offboard_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
